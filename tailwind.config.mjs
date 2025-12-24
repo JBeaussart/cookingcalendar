09bf0b929352673b5e7c24a29d106c32419c7e7d
jeremy beaussart <jeremy.beaussart@gmail.com>
2025-12-24 15:32:23 +0100

    Remplacement de la couleur orange/emerald par vert sauge (#7BAE9D) et mise à jour des dégradés sur la landing page

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        sage: {
+          50: '#f0f7f5',
+          100: '#d9ede8',
+          200: '#b7ddd4',
+          300: '#7BAE9D',
+          400: '#6a9d8c',
+          500: '#7BAE9D',
+          600: '#5d8a7a',
+          700: '#4d7063',
+          800: '#425c52',
+          900: '#394d45',
+        },
+      },
+    },
+  },
+};
+
